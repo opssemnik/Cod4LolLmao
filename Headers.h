69368b0a147497d66c19a3c2057455398cc41614
antonio <antoniobvg@gmail.com>
2014-01-27 17:49:16 -0200

    Revert "Preparando som"
    This reverts commit f7b484ce851ef4731a04694519c6a21f850d167f.

--- Headers.h
+++ Headers.h
@@ -1,10 +1,9 @@
 
 #ifndef HEADERS_H 
 #define HEADERS_H 
-#pragma once
-#define WIN32_LEAN_AND_MEAN
-#define WIN32_EXTRA_LEAN
+
 #pragma warning( disable: 4996 )
+
 #include <windows.h>
 #include <math.h>
 #include <time.h>
@@ -16,9 +15,9 @@
 #include <ddraw.h>
 #include <d3d9.h>
 #include <d3dx9.h>
-#include <stdlib.h>;
-#pragma comment (lib, "d3d9.lib" )
-#pragma comment (lib, "d3dx9.lib" )
+#pragma comment ( lib, "d3d9.lib" )
+#pragma comment ( lib, "d3dx9.lib" )
 #include "Engine.h"
 #include "Tools.h"
+
 #endif
